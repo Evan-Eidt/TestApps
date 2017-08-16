30ab7f102adeb786f2995539c34d8cfee2031ea0
Evan-Eidt <evanmeidt@gmail.com>
2017-08-15 21:03:43 -0400

    Added Menu button to calendar.

--- app/src/main/java/com/example/evanm/testapps/CalendarPage.java
+++ app/src/main/java/com/example/evanm/testapps/CalendarPage.java
@@ -1,7 +1,10 @@
 package com.example.evanm.testapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalendarPage extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class CalendarPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_page);
+
+        Button menuButton = (Button) findViewById(R.id.menuButton);
+
+            menuButton.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+
+          public void onClick(View v) {
+
+                goToMenu();
+
+            }
+
+        });
+}
+    private void goToMenu() {
+
+        Intent intent = new Intent(this, MenuScreen.class);
+
+        startActivity(intent);
+
     }
 }
