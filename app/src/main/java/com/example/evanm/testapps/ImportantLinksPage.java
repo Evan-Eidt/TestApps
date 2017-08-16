a0212abcd1c8dd91c2696ee34ec713dcb7569699
Evan-Eidt <evanmeidt@gmail.com>
2017-08-15 20:56:33 -0400

    Added Calendar, Home icon in menu, and other updates.

--- app/src/main/java/com/example/evanm/testapps/ImportantLinksPage.java
+++ app/src/main/java/com/example/evanm/testapps/ImportantLinksPage.java
@@ -1,6 +1,7 @@
 package com.example.evanm.testapps;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,42 @@ public class ImportantLinksPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_important_links_page);
+        Button menuButton = (Button) findViewById(R.id.menuButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                goToMenu();
+
+            }
+
+        });
+    }
+    private void goToMenu() {
+
+        Intent intent = new Intent(this, MenuScreen.class);
+
+        startActivity(intent);
 
     }
+
+    public void goToCHS (View view) {
+        goToUrl ( "https://sites.google.com/a/chelsea.k12.mi.us/chelseahigh/");
+    }
+
+    public void goToPS (View view) {
+        goToUrl ( "https://ps1.chelsea.k12.mi.us/public/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
 }
+
+
--- app/src/main/java/com/example/evanm/testapps/MenuScreen.java
+++ app/src/main/java/com/example/evanm/testapps/MenuScreen.java
@@ -13,7 +13,7 @@ public class MenuScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
-        ImageButton links = (ImageButton) findViewById(R.id.importantLinks);
+        ImageButton links = (ImageButton) findViewById(R.id.important_links);
 
         links.setOnClickListener(new View.OnClickListener() {
 
@@ -26,10 +26,48 @@ public class MenuScreen extends AppCompatActivity {
             }
 
         });
+
+        ImageButton home = (ImageButton) findViewById(R.id.home);
+
+            home.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    goToHome();
+
+        }
+
+    });
+
+        ImageButton calendar = (ImageButton) findViewById(R.id.calendar);
+
+            calendar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    goToCalendar();
+
+                }
+
+    });
+}
+    private void goToCalendar() {
+
+        Intent intent = new Intent(this, CalendarPage.class);
+
+        startActivity(intent);
+
     }
     private void goToLinks() {
 
         Intent intent = new Intent(this, ImportantLinksPage.class);
+
+        startActivity(intent);
+
+    }
+    private void goToHome() {
+
+        Intent intent = new Intent(this, SchoolHome.class);
 
         startActivity(intent);
 
